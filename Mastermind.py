aa70364ae3a57dbe5e63126430ae95c38e0cd005
Oliver Gorst <oliver.gorst@outlook.com>
2022-08-27 00:17:30 -0400

    setting the solution

--- Mastermind.py
+++ Mastermind.py
@@ -1,8 +1,23 @@
-#Start test
-#Next line
+import random
 
 #The six possible symbols will be ~, !, @, #, $, and %
-
 possible_options = ['~', '!', '@', '#', '$', '%']
+
+#create a tuple of 4 items, pulling a random item from the list above for each item
+
+item1 = random.choice(possible_options)
+item2 = random.choice(possible_options)
+item3 = random.choice(possible_options)
+item4 = random.choice(possible_options)
+
+game_solution = (item1, item2, item3, item4)
+
+
+
+
+
+print(game_solution)
+
+
 
 
