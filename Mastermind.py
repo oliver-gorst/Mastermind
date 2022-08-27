d971941e3054946cb2adbfcfadfd0802d3cb6595
Oliver Gorst <oliver.gorst@outlook.com>
2022-08-27 00:27:25 -0400

    turn counter added

--- Mastermind.py
+++ Mastermind.py
@@ -4,13 +4,17 @@ import random
 possible_options = ['~', '!', '@', '#', '$', '%']
 
 #create a tuple of 4 items, pulling a random item from the list above for each item
-
 item1 = random.choice(possible_options)
 item2 = random.choice(possible_options)
 item3 = random.choice(possible_options)
 item4 = random.choice(possible_options)
-
 game_solution = (item1, item2, item3, item4)
+
+#Setup the turn counter
+turn_counter = 8
+
+
+
 
 
 
