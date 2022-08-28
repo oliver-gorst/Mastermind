210b4eb5eb815828ac3e18c7dbbbc38f2453fb3c
oliver-gorst <oliver.gorst@outlook.com>
2022-08-28 00:41:14 -0400

    Add more structure

--- Mastermind.py
+++ Mastermind.py
@@ -10,8 +10,12 @@ item3 = random.choice(possible_options)
 item4 = random.choice(possible_options)
 game_solution = (item1, item2, item3, item4)
 
+
+
 #Setup the turn counter
 turn_counter = 8
+
+
 
 #Player 1 enter your name
 player1_name = input("Player 1 enter your name.")
@@ -20,11 +24,33 @@ player2_name = input("Player 2 enter your name.")
 
 
 #Do we want to put the entire game within a big while loop for the turn counter?
-#Ask player to input a guess
-print("%s input your guess. Enter four characters separated by a space."%player1_name)
+#Ask player to input a guess 
+print("{} input your guess. Enter four characters from ~, !, @, #, $, and %".format(player1_name))
+
+#Do we need to check to make sure that there are only and exactly 4 characters, and that they are from the list of possible options
+guess = input("")
+
+#Validate the input from the user
+#for characters in guess:
+#    if characters != '~' or '!' or '@' or '#' or '$' or '%':
+#        print("No, I won't run")
+#    else:
+#        continue
+
+#Convert input 4 character text into tuple
+tuple_guess = tuple(guess)
+
+#Setup the initial hit and blow count
+HitCount = 0
+BlowCount = 0
+
+#Print the guess, so the history can be shown of each guess and results combination
+print(tuple_guess)
+
+#For each guess, figure out if its in the game solution, and if it is in the right space
 
 
-#print(game_solution)
+
 
 
 
