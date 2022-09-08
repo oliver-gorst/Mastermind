9c98a59e4e7c157b100e906f2afb0ebddf226114
Oliver Gorst <oliver.gorst@outlook.com>
2022-09-08 11:48:58 -0400

    copy cat file

--- variablePlayground.py
+++ variablePlayground.py
@@ -1,7 +1,7 @@
 import random
 
 #The six possible symbols will be ~, !, @, #, $, and %
-PossibleOptions = ['~', '!', '@', '#', '$', '%']
+PossibleOptions = ['A', 'B', 'C', 'D', 'E', 'F']
 
 #Create a list of 4 items, pulling a random item from the list above for each item
 item1 = random.choice(PossibleOptions)
@@ -12,3 +12,11 @@ MeowGameSolution = [item1, item2, item3, item4]
 
 
 print(MeowGameSolution)
+
+
+CopyCatSolution = MeowGameSolution
+
+
+print(CopyCatSolution)
+
+
